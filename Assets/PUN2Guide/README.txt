eb4e62c490b1c618c525c082065c2eec2f077feb
Ohyoung Song <songohyoung@gmail.com>
2024-08-09 14:41:47 +0900

    init commit

---
+++ Assets/PUN2Guide/README.txt
@@ -0,0 +1,15 @@
+PUN2 Guide by Sharp Coder
+
+This project contains an example multiplayer game using Photon's PUN2 API.
+
+It consists of 2 folders:
+
+'Photon' is a PUN2 API which was downloaded from the official source ( https://assetstore.unity.com/packages/tools/network/pun-2-free-119922?aid=1011l4LKS )
+'PUN2Guide' contains a simple multiplayer game example:
+
+- Before begin make sure to setup your Photon APP ID (Find out more how to do it here: https://sharpcoderr.com/blog/make-a-multiplayer-game-in-unity-3d-using-pun-2 )
+- Add both 'GameLobby' and 'GameLevel' scenes from PUN2Guide/Scenes folder to the Build settings (GameLobby should be first in list)
+- Open 'GameLobby' scene to launch an example scene
+- Press Play then click 'Create Room' or join the room if there any
+
+sharpcoderr.com
---
+++ Assets/PUN2Guide/Scripts/SimplePlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Front/Back
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * Time.deltaTime * 2.45f, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-transform.forward * Time.deltaTime * 2.45f, Space.World);
+        }
+
+        //Rotate Left/Right
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -14, 0) * Time.deltaTime * 4.5f, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * 4.5f, Space.Self);
+        }
+    }
+}
